7adb9beaab3e5b972b7b8ed9ac320c7d988e3cab
Ciino <derekacibar20@gmail.com>
2021-08-09 15:46:47 +0800

    Added login page, RBAC and a new model 'admin_users' for admin accounts

--- src/admin.router.js
+++ src/admin.router.js
@@ -1,10 +1,52 @@
 const AdminBro = require('admin-bro');
-const { buildRouter } = require('@admin-bro/express');
+const    AdminExpress  = require('@admin-bro/express');
 const express = require('express');
+const { AdminUser } = require('../models/admin_users');
+const bcrypt = require('bcrypt');
 
 const buildAdminRouter = (admin) => {
-  const router = buildRouter(admin);
+const router = AdminExpress.buildAuthenticatedRouter(admin,{
+  authenticate: async (email, password) => {
+  const user = await AdminUser.findOne({ email })
+  if (user) {
+    const matched = await bcrypt.compare(password, user.encryptedPassword)
+    if (matched) {
+      return user
+    }
+  }
+  return false
+  },
+  cookieName: "PLMAT Admin",
+  cookiePassword: 'some-secret-password-used-to-secure-cookie',
+})
   return router;
 };
 
 module.exports = buildAdminRouter;
+
+
+// const AdminBro = require('admin-bro');
+// const    AdminExpress  = require('@admin-bro/express');
+// const express = require('express');
+// const { AdminUser } = require('../models/admin_users');
+// const bcrypt = require('bcrypt');
+
+// const buildAdminRouter = (admin) => {
+// const router = AdminExpress.buildAuthenticatedRouter(admin,{
+//   authenticate: async (email, password) => {
+//   const user = await AdminUser.findOne({ email })
+//   if (user) {
+//     const matched = await bcrypt.compare(password, user.encryptedPassword)
+//     if (matched) {
+//       return user
+//     }
+//   }
+//   return false
+//   },
+//   cookieName: "PLMAT Admin",
+//   cookiePassword: 'some-secret-password-used-to-secure-cookie',
+// })
+//   return router;
+// };
+
+// module.exports = buildAdminRouter;
